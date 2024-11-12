3d988e7f1e9bbfe596b5e20d4fce5359672016b3
jcq <chunqiao21@gmail.com>
2024-11-12 09:39:50 -0800

    review

---
+++ index.js
@@ -0,0 +1,39 @@
+// review
+function insertSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    for (let j = i - 1; j >= 0 && arr[j] > arr[j + 1]; j--) {
+      swap(arr, j, j + 1);
+    }
+  }
+  return arr;
+}
+
+function bubbleSort(arr) {
+  for (let i = arr.length - 1; i >= 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+      }
+    }
+  }
+  return arr;
+}
+
+function selectSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let minValueIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minValueIndex]) {
+        minValueIndex = j;
+      }
+    }
+    swap(arr, i, minValueIndex);
+  }
+  return arr;
+}
+
+function swap(arr, i, j) {
+  arr[i] = arr[i] ^ arr[j];
+  arr[j] = arr[i] ^ arr[j];
+  arr[i] = arr[i] ^ arr[j];
+}
